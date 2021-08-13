b6e5a30f9b4517fd588e5631f523d95cf17e6baa
Kostas Zafolias <76820150+Daewooer@users.noreply.github.com>
2021-08-14 00:00:02 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+
+print("pc finder advanced booting up")
+print("tell us your budget (300$-80$0)")
+user_input = input()
+if user_input == "300$":
+	print("Ryzen 3 3200G\nMSI ProSeries AMD A320\nCrucial Ballistix BL2K4G24C16U4B 2400 MHz\nSilicon Power SSD 256GB 3D NAND A55 SL\nVetroo darkFlash\nGIM RGB")
+
+elif user_input == "400$":
+	print("AMD Ryzen 5 5600X\nMSI ProSeries AMD Ryzen 2ND and 3rd Gen AM4 M.2 USB 3 DDR4 D-Sub DVI HDMI micro-ATX\nPatriot Memory PV48G300C6K VIPER 4 Series 3000 MHz 8 GB\nKingston SSD A400\nEvga 100-Br-0600-K3 Psu Br 80+ Bronze 600W\nDEEPCOOL MATREXX 50 Mid")
+
+elif user_input == "500$":
+	print("AMD Ryzen 3 2200G\nASUS Prime B450M\nPatriot Viper Elite Series DDR4 8GB (2x4GB) 2666MHz\nPowerColor VGA - AXRX 570 4GB\nInland Professional 120GB SATA II\nDeepCool MATREXX 55 Midi-Tower Black\nEVGA 100-B1-0500-KR - 500W 80 Plus Bronze")
+
+elif user_input == "600$":
+	print("Intel Core i3-8100\nASRock MB H310M-G\nTEAMGROUP T-Force Vulcan DDR4 8GB (2x4GB) 2400MHz\nTeam Group Delta RGB SSD (Magnificent Version) Black 250GB\nCougar TURRET Mid Tower\nCorsair RMX Series RM750x ")
+
+elif user_input == "750$":
+	print("AMD Ryzen 5 1600\nASROCK B450M STEEL LEGEND\nCorsair Vengeance RGB PRO 16 GB (2 x 8 GB) DDR4 2666 MHz\nEVGA GeForce GTX 1660 Ti\nnland Professional 480GB\nGigabyte 28200-p650b-1eur - 650W 12V Power Supply\nNZXT S340 Mid Tower Case ")
+
+elif user_input == "800$":
+	print("ASUS GeForce GTX 1660 Super Overclocked 6GB\nAMD Ryzen 5 3600 6-Core, 12-Thread Unlocked\nB450 TOMAHAWK MAX II\nCorsair Vengeance LPX 16GB (2x8GB) DDR4 DRAM 3200MHz\nEVGA 110-BQ-0600-K1 600 BQ, 80+ Bronze 600W\nWestern Digital 500GB\nARCTIC P12 PWM\nJBtek All Black Sleeved PWM Fan Splitter Cable 1 to 2 Converter, 2 Pack\nNZXT H510")
